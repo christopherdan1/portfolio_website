2adcf59d8269a28ec39bb4e951482b067ec8f44e
christopherdan1 <65066462+christopherdan1@users.noreply.github.com>
2022-04-26 00:45:01 -0400

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -21,9 +21,8 @@ const Hero = (props) => (
           The purpose of {site} is to showcase valuable design and development practices to assist with modern business problems.
           Let's take your project to the next level and build something awesome.
         </SectionText>
-<LinkedItem href="mailto:christopher@holdersolutions.us">
-        <Button>Learn More</Button>
-</LinkedItem>     
+        <Button><LinkedItem href="mailto:christopher@holdersolutions.us">Learn More</LinkedItem></Button>
+   
 </LeftSection>
     </Section>
   </>
