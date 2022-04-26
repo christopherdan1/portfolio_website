52deeac5037ec3d2ec4a7ea7004646e897fc63ef
christopherdan1 <65066462+christopherdan1@users.noreply.github.com>
2022-04-26 00:26:23 -0400

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -21,7 +21,7 @@ const Hero = (props) => (
           The purpose of {site} is to showcase valuable design and development practices to assist with modern business problems.
           Let's take your project to the next level and build something awesome.
         </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
+        <Button onClick="location.href='location.href='mailto:christopher@holdersolutions.us';">Learn More</Button>
       </LeftSection>
     </Section>
   </>
