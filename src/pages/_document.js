e552f2c6b82b0383097b42f1d486203abe2b8a5a
christopherdan1 <65066462+christopherdan1@users.noreply.github.com>
2022-04-28 01:16:37 -0400

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import Meta from './constants/Meta.js'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
