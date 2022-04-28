1333c4326aa58473f65c1d4cdbfe72d80045c03c
christopherdan1 <65066462+christopherdan1@users.noreply.github.com>
2022-04-28 01:12:13 -0400

    Create Meta.js

---
+++ src/constants/Meta.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+const Meta = ({title, keywords, description, ogTitle, ogType, ogUrl, ogImage}) => {
+    return ( 
+       <Head>
+           <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+           <meta name="keywords" content={keywords}></meta>
+           <meta name="description" content={description}></meta>
+           <meta property="og:title" content={ogTitle} />
+           <meta property="og:type" content={ogType}" />
+           <meta property="og:url" content={ogUlr} />
+           <meta property="og:image" content={ogImage} />
+           <meta charSet="utf-8"></meta>
+           <link rel="icon" href="/favicon.ico"></link>
+           <title>{title}</title>
+       </Head>
+     );
+}
+ Meta.defaultProps = {
+     title: "AI writer",
+     keywords: "ai content writer, ai writer",
+     description: "latest AI writer",
+     ogTitle: "AI writer"
+ }
+export default Meta;
