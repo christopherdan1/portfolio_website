566b9fc9cc79faced15190763c9bacdcd577cfe6
christopherdan1 <65066462+christopherdan1@users.noreply.github.com>
2022-05-10 23:31:49 -0400

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,7 +6,7 @@ export const projects = [
     tags: ["Figma", "Design"],
     source: "https://www.figma.com/proto/xSbi3ZDTKFddI4o11QkjlQ/Joplin-Logistics-LLC?node-id=2%3A2&scaling=scale-down&page-id=0%3A1",
     visit: "https://www.figma.com/proto/xSbi3ZDTKFddI4o11QkjlQ/Joplin-Logistics-LLC?node-id=2%3A2&scaling=scale-down&page-id=0%3A1",
-    id: 0,
+    id: 1,
   },
     {
     title: "Honda Launch UI",
@@ -15,7 +15,7 @@ export const projects = [
     tags: ["Figma", "JavaScript"],
     source: "https://www.figma.com/proto/Zy6HxL0aDVCxG4VOhGssmJ/An-Awesome-Portfolio?node-id=24%3A40&scaling=scale-down&page-id=15%3A2&starting-point-node-id=24%3A40&show-proto-sidebar=1",
     visit: "https://www.figma.com/proto/Zy6HxL0aDVCxG4VOhGssmJ/An-Awesome-Portfolio?node-id=24%3A40&scaling=scale-down&page-id=15%3A2&starting-point-node-id=24%3A40&show-proto-sidebar=1",
-    id: 1,
+    id: 2,
   },
   {
     title: "Netflix Animation UI",
@@ -25,7 +25,7 @@ export const projects = [
     tags: ["Figma", "UI"],
     source: "https://www.figma.com/proto/LIVfod9FI9I9O9kMUDbhe9/Netflix-New-Design?node-id=5%3A260&scaling=scale-down&page-id=0%3A1&starting-point-node-id=3%3A72",
     visit: "https://www.figma.com/proto/LIVfod9FI9I9O9kMUDbhe9/Netflix-New-Design?node-id=5%3A260&scaling=scale-down&page-id=0%3A1&starting-point-node-id=3%3A72",
-    id: 2,
+    id: 3,
   },
    {
     title: "Landing Page - CPO ",
@@ -35,7 +35,7 @@ export const projects = [
     tags: ["Figma", "SquareSpace", "UI"],
     source: "https://www.figma.com/proto/kiJ74eTVOADi5o8I4QKGnD/ReDesigns?node-id=23%3A8750&scaling=scale-down-width&page-id=23%3A8749&starting-point-node-id=23%3A8750",
     visit: "https://www.figma.com/proto/kiJ74eTVOADi5o8I4QKGnD/ReDesigns?node-id=23%3A8750&scaling=scale-down-width&page-id=23%3A8749&starting-point-node-id=23%3A8750",
-    id: 3,
+    id: 4,
   },
   {
     title: "Kingdom Hearts UI",
@@ -45,7 +45,7 @@ export const projects = [
     tags: ["Figma", "Design", "Prototype"],
     source: "https://www.figma.com/proto/9ztLhEhCWzLA5cuQCBHaRi/Kingdom-Hearts-Menu?node-id=108%3A114&scaling=scale-down&page-id=101%3A2&starting-point-node-id=108%3A43",
     visit: "https://www.figma.com/proto/9ztLhEhCWzLA5cuQCBHaRi/Kingdom-Hearts-Menu?node-id=108%3A114&scaling=scale-down&page-id=101%3A2&starting-point-node-id=108%3A43",
-    id: 4,
+    id: 5,
   },
   {
     title:" Disney Clone",
@@ -54,7 +54,7 @@ export const projects = [
     tags:["ReactJS", "Front-End","Prototype"],
     source:"https://disneyplus-clone-f8077.web.app/ ",
     visit:" https://disneyplus-clone-f8077.web.app/",
-    id:5,
+    id:6,
   },
  {
     title:" Whatsapp Clone",
@@ -63,7 +63,7 @@ export const projects = [
     tags:["ReactJS", "Development","Prototype"],
     source:"https://whatsapp-web-clone-c6268.firebaseapp.com/ ",
     visit:" https://whatsapp-web-clone-c6268.firebaseapp.com/",
-    id:6,
+    id:7,
   } 
 ];
 
